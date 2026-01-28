6dbd04f844b56fb5785b22162aa06d185e8f36dd
David Santana <davidsantana06@hotmail.com>
2026-01-27 23:20:37 -0300

    fix(backend — order-item): disable `OUTPUT` clause

--- OrdersAppBackend/Data/AppDbContext.cs
+++ OrdersAppBackend/Data/AppDbContext.cs
@@ -12,5 +12,14 @@ namespace OrdersAppBackend.Data
 
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Disable OUTPUT clause for OrderItems table due to triggers
+            modelBuilder.Entity<OrderItem>()
+                .ToTable(tb => tb.UseSqlOutputClause(false));
+        }
     }
 }
